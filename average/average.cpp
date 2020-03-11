512eb5b04824cb1f13ee1a7ed17fd95732edd3a9
Derya Cortuk <53267226+deryacortuk@users.noreply.github.com>
2020-03-11 13:17:33 +0300

    Pre-Algebra

---
+++ average/average.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+	int x,y,z;
+	float arithmetic;
+	printf("please enter 3 numbers:");
+	scanf("%d %d %d", &x,&y,&z);
+	arithmetic =(x+y+z)/3.0;
+	printf("arithmetic mean:  %.3f",arithmetic);
+	
+	
+}
