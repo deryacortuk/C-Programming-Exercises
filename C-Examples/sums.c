c1620df9fe79d703ed1855f8938433a5c1e88040
Derya Cortuk <53267226+deryacortuk@users.noreply.github.com>
2022-03-06 01:53:39 +0300

    Sum of a two-dimensional array

--- C-Examples/sums.c
+++ C-Examples/sums.c
@@ -2,30 +2,24 @@
 
 int main(){
 	
-	int array[5][5]={{8,3,9,0,10},{3,5,17,1,1},{2,8,6,23,1},{15,7,3,2,9},{6,14,2,6,0} };
-	int i,j;
-	int row[5],column[5];
-	
-	for(i=0;i<5;i++){
-		row[i]=0;
-		for(j=0;j<5;j++){
-			row[i] =row[i] + array[i][j];
-			
-		}
-	}for(i=0;i<5;i++){
-		column[i]=0;
-		for(j=0;j<5;j++){
-			column[i] =column[i] + array[j][i];
-			
-		}
+	int myarray[5][3]={{2,4,6},{1,2,3},{3,6,9},{4,8,12},{5,10,15}};
+    int  (*my_pointer)[3]=myarray;
+    int i,j,column,sum=0;
+    
+    printf("Array has 3 columns.\n");
+    printf("Please enter a column number to calculate the sum of its elements:");
+    scanf("%d",&column);
+    
+    for(i=0;i<5;i++){
+    	
+    	sum += *(*(my_pointer + i)+column-1);
+    	
 	}
+    
+    
+    printf("The sum of elements is:%d ",sum);
+    
 	
-	printf("Row Column\n");
-	for(i=0;i<5;i++){
-		printf("%d  ", row[i]);
-		printf("%d  \n", column[i]);
-	}
+	
 	return 0;
 }
-
-
