deca01428982d576cda42989952a529ebbae8264
Derya Cortuk <53267226+deryacortuk@users.noreply.github.com>
2022-03-13 04:15:20 +0300

    Update reverse_sentence.c

--- C-Examples/reverse_sentence.c
+++ C-Examples/reverse_sentence.c
@@ -18,8 +18,7 @@ int main(void) {
 
     printf("Reversal of sentence: ");
     while (i >= 0) {
-        while (words[--i] != ' ' && i != 0)
-            ;
+        while (words[--i] != ' ' && i != 0);
         j = i == 0 ? 0 : i + 1;
         while (words[j] != ' ' && words[j] != '\0')
             putchar(words[j++]);
